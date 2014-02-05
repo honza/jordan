b0eebb6049dd65070f0c50f821972270895d296a
Honza Pokorny <me@honza.ca>
2014-02-05 06:23:14 +0100

    Fill out project.clj more

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject jordan "0.1.1"
   :description ""
-  :url "http://example.com/FIXME"
+  :url "https://github.com/honza/jordan"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main ^:skip-aot jordan.core
   :target-path "target/%s"
+  :license {:name "BSD" :url "http://opensource.org/licenses/BSD-2-Clause"}
   :profiles {:uberjar {:aot :all}})
